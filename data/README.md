2fe286364793e6ef75854c997bc015b3be8b5a59
default <vnovakovic96@gmail.com>
2019-11-18 17:37:39 +0100

    changed data files

--- data/README.md
+++
@@ -1,1 +0,0 @@
-Corpuses taken from [here](https://github.com/hermitdave/FrequencyWords/tree/master/content/2016)
--- src/wordDifficultyMeter/measurer.mjs
+++ src/wordDifficultyMeter/measurer.mjs
@@ -9,12 +9,14 @@ fs.readFile("../../data/sr_cleaned.txt", 'utf8', (err, data)=>{
         let res = [];
         let cnt = 0;
         for(const word of arr){
-            let diff = measureTextDifficulty(word);
-            cnt ++;
-            if(cnt % 100000 === 0){
-                console.log(cnt)
+            if(word.length >= 3){
+                let diff = measureTextDifficulty(word);
+                cnt ++;
+                if(cnt % 100000 === 0){
+                    console.log(cnt)
+                }
+                res.push(word + " " + diff);
             }
-            res.push(word + " " + diff);
         }
         console.log("measured, writing");
         fs.writeFile("../../data/sr_measured.txt", res.join('\n'), 'utf8', (err)=>{
